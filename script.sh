b3a3d4f6a8d4081a74daf8d2e5579832a00a4c34
leandrotraldi¹p <leandrotraldiperussi@gmail.com>
2024-09-08 14:47:30 -0300

    README e dotfiles

--- script.sh
+++ script.sh
@@ -2,8 +2,8 @@
 
 # comandos para instalação
 
-# primeiros passos
-apt install -y nvidia-driver xinit dbus-x11 
+# nvidia-driver
+apt install -y nvidia-driver
 
 # audio
 apt install wireplumber pipewire-audio-clients-libraries libspa-0.2-jack pulsemixer
@@ -16,6 +16,9 @@ sudo ldconfig
 # reboot
 systemctl reboot
 
+# xorg
+xinit dbus-x11 
+
 # sistema
 apt install -y --no-install-recommends i3-wm i3blocks rofi emacs git  
 
@@ -23,16 +26,18 @@ apt install -y --no-install-recommends i3-wm i3blocks rofi emacs git
 apt install -y --no-install-recommends stow picom redshift xwallpaper htop fonts-jetbrains-mono
 
 # multimidia
-apt install -y --no-install-recommends mpv sxiv zathura transmission-cli rsync
+apt install -y --no-install-recommends mpv sxiv zathura rsync transmission-cli 
 
 # stow
 cd $HOME/pcstation
-stow emacs i3 i3blocks xinit picom redshift rofi gtk
+stow emacs i3 i3blocks xinit picom redshift rofi gtk bash wallpapers
 
 # flatpak
 apt install -y --no-install-recommends flatpak
 flatpak remote-add --if-not-exists --user flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install --user flatseal firefox ungoogled-chromium steam lutris discord reaper 
+
+
 
 
 
